c451e23ed6ee5e77f1e8d3e7d9d5ef8870db4cc3
Anwesh43 <anweshthecool0@gmail.com>
2016-11-18 16:10:32 +0530

    Adding Demo Changes

--- app/src/main/java/com/anwesome/uiview/tinderswiperview/DemoActivity.java
+++ app/src/main/java/com/anwesome/uiview/tinderswiperview/DemoActivity.java
@@ -1,14 +1,65 @@
 package com.anwesome.uiview.tinderswiperview;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.anwesome.uiview.swiperview.SwipeAction;
+import com.anwesome.uiview.swiperview.SwiperLayout;
+import com.anwesome.uiview.swiperview.SwiperObject;
+import com.anwesome.uiview.swiperview.SwiperObjectClickListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by anweshmishra on 18/11/16.
  */
 public class DemoActivity extends AppCompatActivity {
+    private List<SwiperObject> swiperObjects = new ArrayList<>();
+    int like = 0,dislike=0;
+    private final Map<String,Integer> personalitiesMap = new LinkedHashMap<String, Integer>(){{
+        put("Barack",R.drawable.barack);
+        put("Conte",R.drawable.conte);
+        put("Klopp",R.drawable.klopp);
+        put("Putin",R.drawable.putin);
+        put("Wenger",R.drawable.wenger);
+    }};
+    //private final int res[] = {R.drawable.barack,R.drawable.conte,R.drawable.klopp,R.drawable.putin,R.drawable.wenger};
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tinder_swipe_view);
+        for(Map.Entry<String,Integer> entry:personalitiesMap.entrySet()) {
+            SwiperObject swiperObject = SwiperObject.newInstance(BitmapFactory.decodeResource(getResources(),entry.getValue()));
+            final String title = entry.getKey();
+            swiperObject.setListener(new SwiperObjectClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(DemoActivity.this,title,Toast.LENGTH_SHORT).show();
+                }
+            });
+            swiperObject.setSwipeLikeAction(new SwipeAction() {
+                @Override
+                public void doAction() {
+                    like++;
+                    Toast.makeText(DemoActivity.this,"You have liked "+like+" things",Toast.LENGTH_SHORT).show();
+                }
+            });
+            swiperObject.setSwipeDisLikeAction(new SwipeAction() {
+                @Override
+                public void doAction() {
+                    dislike++;
+                    Toast.makeText(DemoActivity.this,"You have disliked "+dislike+" things",Toast.LENGTH_SHORT).show();
+                }
+            });
+            swiperObjects.add(swiperObject);
+        }
+        SwiperLayout swiperLayout = new SwiperLayout(this,swiperObjects);
+        setContentView(swiperLayout);
     }
 }
